3ca695a3fee2e53de30d66b70ee027a2a3bdd43b
nam2626 <nam2626@naver.com>
2023-07-13 14:51:08 +0900

    MVC Mybatis

--- SpringMVCMybatis/src/main/java/com/mvc/controller/MainController.java
+++ SpringMVCMybatis/src/main/java/com/mvc/controller/MainController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mvc.dto.MemberDTO;
 import com.mvc.service.MemberService;
@@ -29,6 +30,15 @@ public class MainController {
 			return "redirect:/main";
 		}
 		return "redirect:/";
+	}
+	
+	@RequestMapping("/main")
+	public ModelAndView memberList(ModelAndView view) {
+		//1. 전체 회원 정보를 DB에서 조회해서 가져옴
+		//     - List로 받음
+		
+		//2. request 영역에 읽어온 전체회원 정보를 저장
+		return view;
 	}
 }
 
