20387da36c9f2bd456df7b3766ed5b5b742f3ed0
nam2626 <nam2626@naver.com>
2023-07-13 11:13:16 +0900

    MVC

--- SpringMVC/src/main/java/com/mvc/MainController.java
+++ SpringMVC/src/main/java/com/mvc/MainController.java
@@ -1,6 +1,10 @@
 package com.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +14,29 @@ public class MainController {
 	public String main() {
 		return "login";
 	}
+	
+	@RequestMapping("/login.do")
+	public String login(HttpServletRequest request) {
+		//아이디 비번 받아서 세션에 저장 id, pass
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("id", id);
+		session.setAttribute("pass", pass);
+		
+		return "login_result";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
