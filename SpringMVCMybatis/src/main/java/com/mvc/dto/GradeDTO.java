1d97a29846b7f391310e924cfe9be5ccde97c671
nam2626 <nam2626@naver.com>
2023-07-13 14:10:00 +0900

    MVC Mybatis

---
+++ SpringMVCMybatis/src/main/java/com/mvc/dto/GradeDTO.java
@@ -0,0 +1,36 @@
+package com.mvc.dto;
+
+public class GradeDTO {
+	private int gradeNo;
+	private String gradeName;
+
+	public GradeDTO(int gradeNo, String gradeName) {
+		this.gradeNo = gradeNo;
+		this.gradeName = gradeName;
+	}
+
+	public GradeDTO() {
+	}
+
+	public int getGradeNo() {
+		return gradeNo;
+	}
+
+	public void setGradeNo(int gradeNo) {
+		this.gradeNo = gradeNo;
+	}
+
+	public String getGradeName() {
+		return gradeName;
+	}
+
+	public void setGradeName(String gradeName) {
+		this.gradeName = gradeName;
+	}
+
+	@Override
+	public String toString() {
+		return "GradeDTO [gradeNo=" + gradeNo + ", gradeName=" + gradeName + "]";
+	}
+
+}
---
+++ SpringMVCMybatis/src/main/java/com/mvc/dto/MemberDTO.java
@@ -0,0 +1,67 @@
+package com.mvc.dto;
+
+public class MemberDTO {
+	private String memberId;
+	private String passwd;
+	private String name;
+	private int age;
+	private char gender;
+	
+	public MemberDTO() {
+	}
+
+	public MemberDTO(String memberId, String passwd, String name, int age, char gender) {
+		this.memberId = memberId;
+		this.passwd = passwd;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO [memberId=" + memberId + ", passwd=" + passwd + ", name=" + name + ", age=" + age
+				+ ", gender=" + gender + "]";
+	}
+	
+}
