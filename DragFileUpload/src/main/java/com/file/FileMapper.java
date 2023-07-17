20b04f923d7f84cef7b3787e8a28449d1e0180fc
nam2626 <nam2626@naver.com>
2023-07-17 10:38:21 +0900

    파일 다운로드

--- DragFileUpload/src/main/java/com/file/FileMapper.java
+++ DragFileUpload/src/main/java/com/file/FileMapper.java
@@ -1,8 +1,13 @@
 package com.file;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FileMapper {
+
+	int insertFile(String absolutePath);
+	List<FileDTO> selectAllFile();
 
 }
