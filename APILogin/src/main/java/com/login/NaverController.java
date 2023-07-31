79f295b87da20c29c9971853a5a0396340d414c7
nam2626 <nam2626@gmail.com>
2023-07-31 12:14:41 +0900

    네아로 예제

--- APILogin/src/main/java/com/login/NaverController.java
+++ APILogin/src/main/java/com/login/NaverController.java
@@ -21,58 +21,71 @@ import org.springframework.web.servlet.ModelAndView;
 public class NaverController {
 	private final String CLIENT_ID = "KHkektMREM4uRRubtXSl";
 	private final String CLIENT_SECRET_ID = "trD4iQ7zjS";
-	
+
 	@RequestMapping("/")
 	public ModelAndView main(HttpSession session, ModelAndView view) throws UnsupportedEncodingException {
 		String clientId = CLIENT_ID;
-	    String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
-	    SecureRandom random = new SecureRandom();
-	    String state = new BigInteger(130, random).toString();
-	    String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
-	    apiURL += "&client_id=" + clientId;
-	    apiURL += "&redirect_uri=" + redirectURI;
-	    apiURL += "&state=" + state;
-	    view.addObject("apiURL", apiURL);
-	    session.setAttribute("state", state);
-	    view.setViewName("naver_login");
+		String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
+		SecureRandom random = new SecureRandom();
+		String state = new BigInteger(130, random).toString();
+		String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+		apiURL += "&client_id=" + clientId;
+		apiURL += "&redirect_uri=" + redirectURI;
+		apiURL += "&state=" + state;
+		view.addObject("apiURL", apiURL);
+		session.setAttribute("state", state);
+		view.setViewName("naver_login");
 		return view;
 	}
-	
+
 	@RequestMapping("/naver/callback")
-	public ModelAndView naverCallBack(HttpSession session, ModelAndView view, 
-			String code, String state ) throws UnsupportedEncodingException, JSONException {
+	public ModelAndView naverCallBack(HttpSession session, ModelAndView view, String code, String state)
+			throws UnsupportedEncodingException, JSONException {
 		String redirectURI = URLEncoder.encode("http://localhost:9999/naver/callback", "UTF-8");
 
 		String apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code" + "&client_id=" + CLIENT_ID
-				+ "&client_secret=" + CLIENT_SECRET_ID 
-				+ "&redirect_uri=" + redirectURI 
-				+ "&code=" + code + "&state=" + state;
-		
+				+ "&client_secret=" + CLIENT_SECRET_ID + "&redirect_uri=" + redirectURI + "&code=" + code + "&state="
+				+ state;
+
 		String res = requestNaverServer(apiURL, null);
-		
-		if(res != null && !res.equals("")) {
+
+		if (res != null && !res.equals("")) {
 			JSONObject json = new JSONObject(res);
 			session.setAttribute("user", res);
 			session.setAttribute("accessTokken", json.getString("access_token"));
 			session.setAttribute("refreshTokken", json.getString("refresh_token"));
-		}else {
+		} else {
 			view.addObject("res", "로그인 실패");
 		}
-		view.setViewName("naver_login_result");		
+		view.setViewName("naver_login_result");
 
 		return view;
 	}
-	
+
+	@RequestMapping("/naver/profile")
+	public ModelAndView getProfile(ModelAndView view, HttpSession session) {
+		String token = (String) session.getAttribute("accessTokken"); // 네이버 로그인 접근 토큰;
+		String header = "Bearer " + token; // Bearer 다음에 공백 추가
+
+		String apiURL = "https://openapi.naver.com/v1/nid/me";
+
+		String result = requestNaverServer(apiURL, header);
+		
+		view.addObject("userInfo",result);
+		view.setViewName("naver_login_result");
+		return view;
+	}
+
 	public String requestNaverServer(String apiURL, String header) {
 		StringBuilder res = new StringBuilder();
 		try {
 			URL url = new URL(apiURL);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
-			if(header != null && !header.equals("")) {
+			if (header != null && !header.equals("")) {
 				con.setRequestProperty("Authorization", header);
 			}
-				
+
 			int responseCode = con.getResponseCode();
 			BufferedReader br;
 			if (responseCode == 200) { // 정상 호출
@@ -93,17 +106,5 @@ public class NaverController {
 		}
 		return res.toString();
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
