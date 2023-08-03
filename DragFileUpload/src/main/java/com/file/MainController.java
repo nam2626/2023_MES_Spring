c0bc8179a3c39dbd2afd2ab946c0e4fa8c4d63b5
nam2626 <nam2626@gmail.com>
2023-08-03 12:51:04 +0900

    ckeditor

--- DragFileUpload/src/main/java/com/file/MainController.java
+++ DragFileUpload/src/main/java/com/file/MainController.java
@@ -27,7 +27,11 @@ public class MainController {
 	public MainController(FileMapper mapper) {
 		this.mapper = mapper;
 	}
-
+	@RequestMapping("/board_write")
+	public String boardWriteView() {
+		return "board_write";
+	}
+	
 	@RequestMapping("/fileUpload.do")
 	public String fileUpload(@RequestParam(value = "file") MultipartFile[] fileload, HttpServletRequest request) {
 		List<FileDTO> list = null;
@@ -110,6 +114,47 @@ public class MainController {
 		}
 
 		return new ResponseEntity(list, HttpStatus.OK);
+	}
+	
+	@RequestMapping("/editerUpload.do")
+	public ResponseEntity<String> editorfileUpload(@RequestParam(value = "upload") MultipartFile fileload, HttpServletRequest request) {
+		List<FileDTO> list = null;
+		// 파일 업로드할 경로 설정
+		File root = new File("c:\\fileupload");
+		if (!root.exists())
+			root.mkdirs();
+		
+		// 현재 날짜 시간
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss");
+		String date = sdf.format(Calendar.getInstance().getTime());
+		
+		//저장한 파일 경로
+		String fileResult = "";
+		
+		
+			
+			//서버에 파일을 저장할 때는 파일명 날짜시간으로 변경
+			//DB에 저장할 떄는 원본파일명과 변경된 파일명 모두 저장
+			//원본파일명
+			String originFileName = fileload.getOriginalFilename();
+			//저장할 파일명
+			String fileName = date + "_" 
+						+ originFileName.substring(originFileName.lastIndexOf('.'));
+			System.out.println("저장할 파일명 : " + fileName);
+			
+			try {
+				//실제 파일이 업로드 되는 부분
+				File file = new File(root, fileName);
+				fileload.transferTo(file);
+				//파일 정보를 DB에 저장
+				mapper.insertFile(file.getAbsolutePath());
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+			
 	}
 	
 	@RequestMapping("/ajax")
